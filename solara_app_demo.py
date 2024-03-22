a78614e41441987c5ac819381eac707ddada2788
Tomas Mendonca <tomaslrmendonca@tecnico.ulisboa.pt>
2024-03-22 07:33:10 +0100

    routing

--- solara_app_demo.py
+++ solara_app_demo.py
@@ -4,15 +4,13 @@ import ee
 from myMap import MyMap 
 from fwiMap import FWIMap
 from fireMap import FireMap
-from home import Home
 
 ee.Authenticate(auth_mode='gcloud')
 ee.Initialize(project='digital-yeti-417904')
 print('Earth Engine Initialized')
 
 routes = [
-    solara.Route(path="/", component=Home, label="Home"),
-    solara.Route(path="myMap", component=MyMap, label="MyMapLabel"),
+    solara.Route(path="/", component=MyMap, label="MyMapLabel"),
     solara.Route(path="fwiMap", component=FWIMap, label="FWI Map"),
     solara.Route(path="fireMap", component=FireMap, label="Fire Map")
 ]
